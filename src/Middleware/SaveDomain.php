6e83cc048afd45e9e17e63cd66b967d156c9bb22
Roman Shkabko <r.shkabko@flamix.email>
2024-02-14 10:09:48 +0200

    Fix session domain

--- src/Middleware/SaveDomain.php
+++ src/Middleware/SaveDomain.php
@@ -4,12 +4,16 @@ namespace Flamix\App24Core\Middleware;
 
 use Closure;
 use Flamix\App24Core\Controllers\AuthController;
+use Flamix\App24Core\Controllers\PortalController;
 
 class SaveDomain
 {
     public function handle($request, Closure $next)
     {
-        AuthController::setDomainToSession();
+        $domain = $request->get('DOMAIN', null);
+        if ($domain) {
+            session(['DOMAIN' => $domain]);
+        }
         return $next($request);
     }
 }
