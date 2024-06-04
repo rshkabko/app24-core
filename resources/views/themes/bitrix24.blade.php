d08fa222aef5a31e5a6f3a5024f4c7853ee0c5cb
Roman Shkabko <r.shkabko@flamix.email>
2024-06-04 19:38:26 +0300

    Add JS csrf and sid

--- resources/views/themes/bitrix24.blade.php
+++ resources/views/themes/bitrix24.blade.php
@@ -7,6 +7,9 @@
         {{-- Styles --}}
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         {{-- JavaScript --}}
+        <script>
+            window.Laravel = {!! json_encode(['csrfToken' => csrf_token(), 'sid' => session()->getId()]) !!};
+        </script>
         <script src="//api.bitrix24.com/api/v1/"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
     </head>
