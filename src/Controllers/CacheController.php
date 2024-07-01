5ecb482e28a4ff88165685fb74670b448439c1a2
Roman Shkabko <r.shkabko@flamix.email>
2024-07-01 14:12:19 +0300

    Clear

--- src/Controllers/CacheController.php
+++ src/Controllers/CacheController.php
@@ -3,31 +3,40 @@
 namespace Flamix\App24Core\Controllers;
 
 use App\Exceptions\App24Exception;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Cache;
 
-class CacheController extends Controller
+class CacheController
 {
+    /**
+     * Get cache key. All keys must be expeted in all controllers.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return string
+     */
     public static function key(string $key, mixed $value = ''): string
     {
         return match($key) {
-            'portal_domain' => "portal_app_" . config('app.name') . "_id_{$value}", //TODO: to ID
+            'portal_domain' => "portal_app_" . config('app.name') . "_id_{$value}",
             'portal_id' => "portal_data_{$value}",
         };
     }
 
     /**
-     * Очищаем кэш портала (где получаем все опции)
-     * Нужно проводить после каждого Инсерта/Апдейта
+     * Clear cache for portal. Need to use after update portal data.
      *
-     * @param int $id
+     * @param  int|null  $id
+     * @param  string|null  $domain
      * @return bool
      * @throws App24Exception
      */
-    public static function clearPortalCache(int $id = 0): bool
+    public static function clearPortalCache(?int $id = null, ?string $domain = null): bool
     {
         $id = $id ?: PortalController::getId();
         Cache::forget(self::key('portal_id', $id));
+        if ($domain) {
+            Cache::forget(self::key('portal_domain', $domain));
+        }
         return true;
     }
 }
