7a5a421957ae3a14e6a4ba4377908ec7a77b8d60
Roman Shkabko <r.shkabko@flamix.email>
2026-02-22 10:20:04 +0200

    Install only POST

--- src/routes.php
+++ src/routes.php
@@ -5,7 +5,7 @@ use Flamix\App24Core\Controllers\InstallController;
 
 Route::group(['prefix' => 'app24'], function () {
     // Install
-    Route::any('install', [InstallController::class, 'install'])->middleware(['web', 'SaveDomain'])->name('app24.install');
+    Route::post('install', [InstallController::class, 'install'])->middleware(['web', 'SaveDomain'])->name('app24.install');
     // Delete
     Route::post('uninstall/{hash}', [InstallController::class, 'destroy'])->middleware(['api', 'App24'])->name('app24.uninstall');
 });
